d08d95d04f14f4e06a90f19274d1ccf90623539d
Orion68 <orions68@gmail.com>
2024-09-23 17:59:48 +0100

    Update 23-9-2024 17:59

---
+++ Server/Pruebas/Ejercicio 1/Controllers/ParametersController.cs
@@ -0,0 +1,16 @@
+﻿using Ejercicio_1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ejercicio_1.Controllers
+{
+    public class ParametersController : Controller
+    {
+        public IActionResult Index(string name, int age, int ID)
+        {
+            ViewBag.ID = ID;
+            ViewBag.name = name;
+            ViewBag.age = age;
+            return View();
+        }
+    }
+}
---
+++ Server/Pruebas/Ejercicio 1/Controllers/SaludosController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ejercicio_1.Controllers
+{
+    public class SaludosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Despedida()
+        {
+            return View();
+        }
+    }
+}
