64037f85bccbe00c2d8342d003d68c3eeea09fd8
matelat-daw <cesarmatelat@gmail.com>
2024-09-26 10:43:47 +0100

    Update 26-9-2024 10:44

--- Client/Javascript/Objetos/js/script.js
+++ Client/Javascript/Objetos/js/script.js
@@ -2,16 +2,16 @@ let musicos = [];
 
 function coleccionHardcodeada()
 {
-    let musico1 = {name: "John", surname: "Lennon", group: "The Beatles", age: 40, bday: 1940, instrument: {first: "Guitarra", second: "Voz", third: "", place: 1}};
-    let musico2 = {name: "Paul", surname: "McCartney", group: "The Beatles", age: 38, bday: 1942, instrument: {first: "Bajo", second: "Voz", third: "", place: 2}};
-    let musico3 = {name: "George", surname: "Harrison", group: "The Beatles", age: 37, bday: 1943, instrument: {first: "Guitarra", second: "Voz", third: "Coros", place: 3}};
-    let musico4 = {name: "Ringo", surname: "Start", group: "The Beatles", age: 40, bday: 1940, instrument: {first: "Batería", second: "Voz", third: "Coros", place: 4}};
-    let musico5 = {name: "Freddie", surname: "Mercury", group: "Queen", age: 34, bday: 1946, instrument: {first: "Piano", second: "Voz", third: "", place: 1}};
-    let musico6 = {name: "Brian", surname: "May", group: "Queen", age: 33, bday: 1947, instrument: {first: "Guitarra", second: "Voz", third: "", place: 2}};
-    let musico7 = {name: "Roger", surname: "Taylor", group: "Queen", age: 31, bday: 1949, instrument: {first: "Batería", second: "Coros", third: "", place: 3}};
-    let musico8 = {name: "John", surname: "Deacon", group: "Queen", age: 29, bday: 1951, instrument: {first: "Bajo", second: "Coros", third: "", place: 4}};
-    let musico9 = {name: "Paul", surname: "Simon", group: "Simon & Garfunkel", age: 39, bday: 1941, instrument: {first: "Guitarra", second: "Piano", third: "Voz", place: 1}};
-    let musico10 = {name: "Arthur", surname: "Garfunkel", group: "Simon & Garfunkel", age: 39, bday: 1941, instrument: {first: "Guitarra", second: "Piano", third: "Voz", place: 1}};
+    let musico1 = {name: "John", surname: "Lennon", group: "The Beatles", age: 40, bday: 1940, instruments: {first: "Guitarra", second: "Voz", third: "", place: 1}};
+    let musico2 = {name: "Paul", surname: "McCartney", group: "The Beatles", age: 38, bday: 1942, instruments: {first: "Bajo", second: "Voz", third: "", place: 2}};
+    let musico3 = {name: "George", surname: "Harrison", group: "The Beatles", age: 37, bday: 1943, instruments: {first: "Guitarra", second: "Voz", third: "Coros", place: 3}};
+    let musico4 = {name: "Ringo", surname: "Start", group: "The Beatles", age: 40, bday: 1940, instruments: {first: "Batería", second: "Voz", third: "Coros", place: 4}};
+    let musico5 = {name: "Freddie", surname: "Mercury", group: "Queen", age: 34, bday: 1946, instruments: {first: "Piano", second: "Voz", third: "", place: 1}};
+    let musico6 = {name: "Brian", surname: "May", group: "Queen", age: 33, bday: 1947, instruments: {first: "Guitarra", second: "Voz", third: "", place: 2}};
+    let musico7 = {name: "Roger", surname: "Taylor", group: "Queen", age: 31, bday: 1949, instruments: {first: "Batería", second: "Coros", third: "", place: 3}};
+    let musico8 = {name: "John", surname: "Deacon", group: "Queen", age: 29, bday: 1951, instruments: {first: "Bajo", second: "Coros", third: "", place: 4}};
+    let musico9 = {name: "Paul", surname: "Simon", group: "Simon & Garfunkel", age: 39, bday: 1941, instruments: {first: "Guitarra", second: "Piano", third: "Voz", place: 1}};
+    let musico10 = {name: "Arthur", surname: "Garfunkel", group: "Simon & Garfunkel", age: 39, bday: 1941, instruments: {first: "Guitarra", second: "Piano", third: "Voz", place: 1}};
 
     musicos.push(musico1);
     musicos.push(musico2);
@@ -38,11 +38,11 @@ function coleccionPorCodigo()
     musico1.group = "The Beatles";
     musico1.age = 40;
     musico1.bday = 1940;
-    musico1.instrument = {};
-    musico1.instrument.first = "Guitarra";
-    musico1.instrument.second = "Voz";
-    musico1.instrument.third = "";
-    musico1.instrument.place = 1;
+    musico1.instruments = {};
+    musico1.instruments.first = "Guitarra";
+    musico1.instruments.second = "Voz";
+    musico1.instruments.third = "";
+    musico1.instruments.place = 1;
 
     let musico2 = {};
     musico2.name = "Paul";
@@ -50,11 +50,11 @@ function coleccionPorCodigo()
     musico2.group = "The Beatles";
     musico2.age = 38;
     musico2.bday = 1942;
-    musico2.instrument = {};
-    musico2.instrument.first = "Bajo";
-    musico2.instrument.second = "Voz";
-    musico2.instrument.third = "";
-    musico2.instrument.place = 2;
+    musico2.instruments = {};
+    musico2.instruments.first = "Bajo";
+    musico2.instruments.second = "Voz";
+    musico2.instruments.third = "";
+    musico2.instruments.place = 2;
 
     let musico5 = {};
     musico5.name = "Freddie";
@@ -62,11 +62,11 @@ function coleccionPorCodigo()
     musico5.group = "Queen";
     musico5.age = 34;
     musico5.bday = 1946;
-    musico5.instrument = {};
-    musico5.instrument.first = "Piano";
-    musico5.instrument.second = "Voz";
-    musico5.instrument.third = "";
-    musico5.instrument.place = 1;
+    musico5.instruments = {};
+    musico5.instruments.first = "Piano";
+    musico5.instruments.second = "Voz";
+    musico5.instruments.third = "";
+    musico5.instruments.place = 1;
 
     let musico6 = {};
     musico6.name = "Brian";
@@ -74,11 +74,11 @@ function coleccionPorCodigo()
     musico6.group = "Queen";
     musico6.age = 33;
     musico6.bday = 1947;
-    musico6.instrument = {};
-    musico6.instrument.first = "Guitarra";
-    musico6.instrument.second = "Voz";
-    musico6.instrument.third = "";
-    musico6.instrument.place = 2;
+    musico6.instruments = {};
+    musico6.instruments.first = "Guitarra";
+    musico6.instruments.second = "Voz";
+    musico6.instruments.third = "";
+    musico6.instruments.place = 2;
 
     let musico3 = {};
     musico3["name"] = "George";
@@ -86,11 +86,11 @@ function coleccionPorCodigo()
     musico3["group"] = "The Beatles";
     musico3["age"] = 37;
     musico3["bday"] = 1943;
-    musico3["instrument"] = {};
-    musico3["instrument"].first = "Guitarra";
-    musico3["instrument"].second = "Voz";
-    musico3["instrument"].third = "Coros";
-    musico3["instrument"].place = 3;
+    musico3["instruments"] = {};
+    musico3["instruments"]["first"] = "Guitarra";
+    musico3["instruments"]["second"] = "Voz";
+    musico3["instruments"]["third"] = "Coros";
+    musico3["instruments"]["place"] = 3;
 
     let musico4 = {};
     musico4["name"] = "Ringo";
@@ -98,11 +98,11 @@ function coleccionPorCodigo()
     musico4["group"] = "The Beatles";
     musico4["age"] = 3
     musico4["bday"] = 40;
-    musico4["instrument"] = {};
-    musico4["instrument"].first = "Batería";
-    musico4["instrument"].second = "Voz";
-    musico4["instrument"].third = "Coros";
-    musico4["instrument"].place = 4;
+    musico4["instruments"] = {};
+    musico4["instruments"]["first"] = "Batería";
+    musico4["instruments"]["second"] = "Voz";
+    musico4["instruments"]["third"] = "Coros";
+    musico4["instruments"]["place"] = 4;
 
     let musico7 = {};
     musico7["name"] = "Roger";
@@ -110,11 +110,11 @@ function coleccionPorCodigo()
     musico7["group"] = "Queen";
     musico7["age"] = 31;
     musico7["bday"] = 1949;
-    musico7["instrument"] = {};
-    musico7["instrument"].first = "Guitarra";
-    musico7["instrument"].second = "Voz";
-    musico7["instrument"].third = "Coros";
-    musico7["instrument"].place = 3;
+    musico7["instruments"] = {};
+    musico7["instruments"]["first"] = "Guitarra";
+    musico7["instruments"]["second"] = "Voz";
+    musico7["instruments"]["third"] = "Coros";
+    musico7["instruments"]["place"] = 3;
 
     let musico8 = {};
     musico8["name"] = "John";
@@ -122,11 +122,11 @@ function coleccionPorCodigo()
     musico8["group"] = "Queen";
     musico8["age"] = 29
     musico8["bday"] = 1951;
-    musico8["instrument"] = {};
-    musico8["instrument"].first = "Batería";
-    musico8["instrument"].second = "Voz";
-    musico8["instrument"].third = "Coros";
-    musico8["instrument"].place = 4;
+    musico8["instruments"] = {};
+    musico8["instruments"]["first"] = "Batería";
+    musico8["instruments"]["second"] = "Voz";
+    musico8["instruments"]["third"] = "Coros";
+    musico8["instruments"]["place"] = 4;
 
     let musico9 = {};
     musico9.name = "Paul";
@@ -134,11 +134,11 @@ function coleccionPorCodigo()
     musico9.group = "Simon & Garfunkel";
     musico9.age = 39;
     musico9.bday = 1941;
-    musico9.instrument = {};
-    musico9.instrument.first = "Guitarra";
-    musico9.instrument.second = "Piano";
-    musico9.instrument.third = "Voz";
-    musico9.instrument.place = 1;
+    musico9.instruments = {};
+    musico9.instruments.first = "Guitarra";
+    musico9.instruments.second = "Piano";
+    musico9.instruments.third = "Voz";
+    musico9.instruments.place = 1;
 
     let musico10 = {};
     musico10["name"] = "Arthur";
@@ -146,11 +146,11 @@ function coleccionPorCodigo()
     musico10["group"] = "Simon & Garfunkel";
     musico10["age"] = 39;
     musico10["bday"] = 1941;
-    musico10["instrument"] = {};
-    musico10["instrument"].first = "Guitarra";
-    musico10["instrument"].second = "Piano";
-    musico10["instrument"].third = "Voz";
-    musico10["instrument"].place = 1;
+    musico10["instruments"] = {};
+    musico10["instruments"]["first"] = "Guitarra";
+    musico10["instruments"]["second"] = "Piano";
+    musico10["instruments"]["third"] = "Voz";
+    musico10["instruments"]["place"] = 1;
 
     musicos.push(musico1);
     musicos.push(musico2);
@@ -186,6 +186,7 @@ function persist() // Alamacena los datos en LocalStorage.
 function show(group)
 {
     let objeto = JSON.parse(localStorage.getItem("Musicos")); // Para Obtener los datos como un array de Objetos hay que usar la función JSON.parse("Name").
+    result.innerHTML = "";
 
     objeto.forEach(musico => { if(group == musico.group) // Se recorre todo el Objeto y si el Grupo del Objeto es el seleccionado en el selector por el Usuario.
     {
