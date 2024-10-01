5675e12e0b99951755a8d943bced19350dbe137f
matelat-daw <cesarmatelat@gmail.com>
2024-10-01 08:52:50 +0100

    Update 01-09-2024 08:52

--- Server/Pruebas/List of Objects/Models/CharacterModel.cs
+++ Server/Pruebas/List of Objects/Models/CharacterModel.cs
@@ -11,7 +11,7 @@ namespace List_of_Objects.Models
         public string? Name { get; set; }
         [Required(ErrorMessage = "Por Favor escribe la Edad")]
         [Range(0, 100, ErrorMessage = "La Edad Debe Estar Entre 0 y 100 Años"), Display(Name = "Edad")]
-        public int Age { get; set; }
+        public int? Age { get; set; }
         [Required(ErrorMessage = "Por Favor escribe el Trabajo")]
         [StringLength(20, ErrorMessage = "La Ocupación Tiene que Tener Entre 5 y 20 Caracters.", MinimumLength = 5), Display(Name = "Ocupación")]
         public string? Job { get; set; }
