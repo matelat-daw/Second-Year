507b41960c960ff9e3b2caaeae77ca08893d386b
matelat-daw <cesarmatelat@gmail.com>
2024-10-04 11:48:46 +0100

    Update 04-10-2024 11:48

---
+++ Server/Evaluable/MVCDBCodeFirst/Migrations/20241004080017_InitialCreate.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MVCDBCodeFirst.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Car",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Model = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    Brand = table.Column<string>(type: "nvarchar(15)", maxLength: 15, nullable: false),
+                    ReleaseDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Car", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Car");
+        }
+    }
+}
---
+++ Server/Evaluable/MVCDBCodeFirst/Models/Car.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVCDBCodeFirst.Models
+{
+    public class Car
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required (ErrorMessage = "Por Favor Escribe el Modelo, Mínimo 3, Máximo 20 Caracteres."), MaxLength(20), MinLength(3), Display(Name = "Modelo")]
+        public string Model { get; set; }
+        [Required(ErrorMessage = "Por Favor Escribe la Marca, Mínimo 3, Máximo 15 Caracteres."), MaxLength(15), MinLength(3), Display(Name = "Marca")]
+        public string Brand { get; set; }
+        [Required(ErrorMessage = "Por Favor Selecciona la Fecha de Fabricación"), Display(Name = "Fecha de Fabricación")]
+        //[Range(typeof(DateTime), "1/1/1970 00:00:00", "1/1/2024 00:00:00", ErrorMessage = "La Fecha Está Fuera de Rango(1/1/1970 a 1/1/2024)")]
+        [DataType(DataType.DateTime)]
+        public DateTime ReleaseDate { get; set; }
+        [Required(ErrorMessage = "Por Favor Escribe el Precio. Debe estar Entre 5000 y 30000 €"), Display(Name = "Precio")]
+        [Range(5000, 30000)]
+        public decimal Price { get; set; }
+    }
+}
