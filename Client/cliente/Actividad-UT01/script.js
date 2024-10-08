3e0aba9c6d1ec599355159da83b674daf2540591
Orion68 <orions68@gmail.com>
2024-10-08 17:43:47 +0100

    Update 08-10-2024 17:43

--- Client/cliente/Actividad-UT01/script.js
+++ Client/cliente/Actividad-UT01/script.js
@@ -1,21 +1,21 @@
 //DOM static selector
 const DOM = {
-    miFormulario: document.getElementById("mi_formulario"),
-    idioma: document.getElementById("idioma"),
-    group: document.getElementById("group"),
-    staff: document.getElementById("staff"),
-    bday: document.getElementById("bday"),
-    miColeccion: document.getElementById("mi_coleccion"),
-    tbody: document.getElementById("tabla_coleccion"),
-    date: document.getElementById("date"),
-    time: document.getElementById("time"),
+    miFormulario: document.getElementById("mi_formulario"), // ID del Formulario.
+    idioma: document.getElementById("idioma"), // Selector de Idioma.
+    group: document.getElementById("group"), // Input que Contiene el Grupo de Música.
+    staff: document.getElementById("staff"), // Input que Contiene el Nombre del Músico.
+    bday: document.getElementById("bday"), // Input con la Fecha de Nacimiento del Músico.
+    miColeccion: document.getElementById("mi_coleccion"), // Selector de Instrumentos de los Músicos.
+    tbody: document.getElementById("tabla_coleccion"), // Cuerpo de la Tabla Donde se muestran los Datos de los Músicos.
+    date: document.getElementById("date"), // Muestra la Fecha en el Idioma Seleccionado por el Usuario.
+    time: document.getElementById("time"), // Muestra el Reloj.
     alerta: document.getElementById("alerta"), // La ID del botón del dialogo.
     title: document.getElementById("title"), // Asigno a la variable title el h4 con id title del Diálogo.
     message: document.getElementById("message"), // Asigno a la variable message el h5 con id message del Díalogo;
-    delete: document.getElementById("delete")
+    delete: document.getElementById("delete") // ID del botón para Borrar un Músico.
 };
 
-function Musico(id, name, group, age, bday, data)
+function Musico(id, name, group, age, bday, data) // Función que se Usa para Crear un Objeto de la Clase Musico.
 {
     this.id = id;
     this.name = name;
@@ -27,26 +27,25 @@ function Musico(id, name, group, age, bday, data)
     this.third = data[2];
 }
 
+let lSM = localStorage.getItem("Musicos"); // ASigna a lSM lo almacenado en localStorage Musicos.
+let lSID = localStorage.getItem("ID"); // Asigna a lSID lo almacenado en localStorage ID.
+if (lSID === null) // Si en localStorage ID no hay nada es null
+{
+    localStorage.setItem("ID", 1); // Le Asigna el Valor 1, Será la Primera ID de los Músicos.
+}
+let musicos = []; // Declaro el Array musicos, contendrá todos los Objertos musico.
+
 (function(){
   // AQUI - Recupera la colección del localStorage y muestralo en la tabla
-  DOM.miFormulario.addEventListener("submit", guardarObjeto);
+    if (lSM !== null) // Si el localStorage Musicos no es null.
+    {
+            musicos = JSON.parse(localStorage.getItem("Musicos")); // Hago el JSON.parse a la cadena almacenada en localStorage Musicos y lo asigno al Array musicos.
+            mostrarObjetoEnTabla(musicos); // Llamo a la función mostrarObjetoEnTabla y le paso el Array con los Músicos.
+    }
+  DOM.miFormulario.addEventListener("submit", guardarObjeto); // Captura el evento click del Botón Submit del Fomulario con ID miFormulario.
 })()
 
-let lS = localStorage.getItem("Musicos");
-let musicos = [];
-let musicoIndex = 1;
-
-if (lS !== null)
-{
-     musicos = JSON.parse(localStorage.getItem("Musicos"));
-     let len = musicos.length;
-     let next = musicos[len - 1].id;
-     musicoIndex = next;
-     musicoIndex++;
-     mostrarObjetoEnTabla(musicos);
-}
-
-function guardarObjeto(e)
+function guardarObjeto(e) // Función que Guarda los Objetos, Recive el Evento.
 {
     let options = DOM.miColeccion.options; // Asigna a la Variable options Todas las Opciones del Selector.
     let len = options.length; // Obtiene la Cantidad de opciones(options es un array y tiene tamaño length).
@@ -63,29 +62,28 @@ function guardarObjeto(e)
         j++; // Incrementa j.
     }
 
-    let id = musicoIndex; // Asigna a id el valor de la Variable musicoIndex;
-    musicoIndex++; // Incrementa en 1 musicoIndex.
-    let mName = DOM.staff.value; // Asigna a mName el contenido del input con ID staff.
+    let ID = parseInt(localStorage.getItem("ID")); // Obtengo en la variable ID la ID de localStorage ID.
+    let mName = DOM.staff.value; // Asigna a mName el contenido del input con ID staff(Nombre del Músico).
     let group = DOM.group.value; // Asigna a group el contenido del input con ID group.
     let bday = DOM.bday.value; // Asigna a bday el contenido del input con ID bday.
     let now = new Date().getFullYear(); // Asigna a now El Corriente Año.
     let yearsOld = now - bday; // Asigna a yearsOld el Resultado de Restar al Año Actual el Año de Nacimiento del Músico.
 
-    let musico = new Musico(id, mName, group, yearsOld, bday, data); // Crea un Nuevo Músico Llamando al Constructor Musico.
+    let musico = new Musico(ID, mName, group, yearsOld, bday, data); // Crea un Nuevo Músico Llamando al Constructor de la Función Musico.
     musicos.push(musico); // Agrega el Músico al Array musicos.
 
     mostrarObjetoEnTabla(musicos); // Muestra en la Tabla la Lista de Músicos.
     // AQUI - Actualizar la colección en el localStorage.
 
+    localStorage.setItem("ID", JSON.stringify(++ID)); // Incrementa localStorage ID, Hay que Preincrementar, si no Nunca se Produce el Incremento.
     localStorage.setItem("Musicos", JSON.stringify(musicos)); // Almacena los Músicos en localStorage.
 
-    DOM.miFormulario.reset();
-    yearsOld = 0;
+    DOM.miFormulario.reset(); // Limpia el Formulario.
 
     e.preventDefault(); // Para evitar el envío del formulario
 }
 
-function borrarObjeto(id, e)
+function borrarObjeto(id) // Función para Eliminar un Músico del Array de Músicos.
 {
   //AQUI - Borrar el objeto de la colección
     // alert("Programa esta función para borrar el objeto con id " + id)
@@ -105,10 +103,10 @@ function borrarObjeto(id, e)
     mostrarObjetoEnTabla(musicos); // Muestra los Músicos en la Tabla.
 }
 
-function mostrarObjetoEnTabla(musicos)
+function mostrarObjetoEnTabla(musicos) // Muestro los Músicos en el Cuerpo de la Tabla(tbody).
 {
-  let tr;
-  let td;
+  let tr; // Fila.
+  let td; // Columna.
   DOM.tbody.innerHTML = ""; // Limpio el Contenido del body de la Tabla Donde Están las Columnas con los Datos de los Músicos.
 
   musicos.forEach(musico => {
@@ -146,15 +144,15 @@ function mostrarObjetoEnTabla(musicos)
         td.textContent = musico.third;
         tr.appendChild(td);
         //
-        DOM.tbody.appendChild(tr);
+        DOM.tbody.appendChild(tr); // Adjunto al Cuerpo de la Tabla la Fila con todas las Columnas Creadas con Todos los Datos de los Músicos.
     });
 }
 
-function checkQtty() // Función que verifica la cantidad de fotos que se subieron para un evento, máximo 3, recibe el array files, el nombre del input.
+function checkQtty() // Función que verifica la cantidad de Instrumentos que se seleccionaron, máximo 3.
 {
     let options = DOM.miColeccion.options;
     let len = options.length;
-    let i = 0;
+    let i = 0; // Cantidad de Items Seleccionados.
     let j = 0; // J contiene el tamaño del selector.
     while (j < len)
     {
@@ -170,7 +168,7 @@ function checkQtty() // Función que verifica la cantidad de fotos que se subier
     }       
 }
 
-function showDate(country)
+function showDate(country) // Función que Muestra la Fecha en el Idioma Seleccionado por el Usuario.
 {
   let localDate = new Intl.DateTimeFormat(country, {
     weekday: "long",
@@ -180,18 +178,17 @@ function showDate(country)
     }).format(new Date());
 
     DOM.date.innerHTML = "La Fecha es: " + localDate;
-    updateTime();
-    setInterval(updateTime, 1000);
+    updateTime(); // Llama a la Función updateTime() que Muestra el Reloj en Pnatalla.
+    setInterval(updateTime, 1000); // Y la Llama cada 1000 milisegundos(1 segundo).
 }
 
-function updateTime()
+function updateTime() // Muestra el Reloj en Pantalla.
 {
   const now = new Date();
   const hours = now.getHours();
   const minutes = now.getMinutes();
   const seconds = now.getSeconds();
 
-  // Format the string with leading zeroes
   const clockStr = `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
 
   DOM.time.innerHTML = clockStr;
@@ -216,5 +213,5 @@ function toast(warn, ttl, msg) // Función para mostrar el Dialogo con los mensa
     }
     DOM.title.innerHTML = ttl; // Muestro el Título del dialogo.
     DOM.message.innerHTML = msg; // Muestro los mensajes en el diálogo.
-    DOM.alerta.click(); // Lo hago aparecer pulsando el botón con ID alerta.
+    DOM.alerta.click(); // Lo hago aparecer Usando la Función click() del botón con ID alerta.
 }
