4655385e83a3eff8788ad3bbdaa0f8cb086c2bfe
Orion68 <orions68@gmail.com>
2024-10-27 11:28:32 +0000

    Update 27-10-2024 11:28

--- Interface/25-10-Colors/js/main.js
+++ Interface/25-10-Colors/js/main.js
@@ -15,7 +15,10 @@ function changeStyle(style)
     {
         case "young":
             DOM.body.style.color = "gray";
-            DOM.body.style.backgroundColor = "black";
+            DOM.body.style.backgroundColor = "blue";
+            DOM.nav.style.backgroundColor = "green";
+            DOM.aside.style.backgroundColor = "green";
+            DOM.footer.style.backgroundColor = "red";
             for (let i = 0; i < DOM.anchor.length; i++)
             {
                 DOM.anchor[i].style.color = "gray";
@@ -29,6 +32,18 @@ function changeStyle(style)
             break;
         case "asset":
             break;
+        case "elegant":
+            break;
         default:
+            DOM.body.style.color = "black";
+            DOM.body.style.backgroundColor = "white";
+            for (let i = 0; i < DOM.anchor.length; i++)
+            {
+                DOM.anchor[i].style.color = "black";
+            }
+            for (let i = 0; i < DOM.list.length; i++)
+            {
+                DOM.list[i].style.color = "black";
+            }
     }
 }
