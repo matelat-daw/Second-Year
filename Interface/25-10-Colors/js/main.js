1202b62860e2387d329dfc730b05b1498575b4cf
Orion68 <orions68@gmail.com>
2024-10-28 19:46:21 +0000

    Update 28-10-2024 19:46

--- Interface/25-10-Colors/js/main.js
+++ Interface/25-10-Colors/js/main.js
@@ -5,26 +5,20 @@ const DOM = {
     aside: document.getElementById("aside"),
     main: document.getElementById("main"),
     select: document.getElementById("style"),
+    list: document.querySelector(".list-group-item"),
+    anchor: document.querySelector(".nav-link"),
     footer: document.getElementById("footer"),
     footer_text: document.getElementById("footer-text"),
-    anchor: document.querySelectorAll("a:not(#email)"),
-    list: document.querySelectorAll('li:not(#aside-li-1):not(#aside-li-2):not(#aside-li-3):not(#aside-li-4)'),
-    // button: document.querySelectorAll("button")
-    button: document.getElementsByClassName("btn")
-}; // Se excluyen los li con ID aside-l1-1 / 4, los que están en el Aside(Ya que el Aside Tiene un Fondo y si se le Aplica a los li del Aside, No Queda Bien) y el enlace del footer con ID email(Pare Verlo Mejor en los Estilos Sobrio y Elegante).
+    button: document.querySelector(".btn")
+};
 
-let body_color = DOM.body.style.color;
-let body_backcolor = DOM.body.style.backgroundColor;
-let header_backcolor = DOM.header.style.backgroundColor;
-let header_color = DOM.header.style.color;
-let nav_color = DOM.nav.style.backgroundColor;
-let aside_color = DOM.nav.style.backgroundColor;
-let main_color = DOM.nav.style.backgroundColor;
-let footer_color = DOM.nav.style.backgroundColor;
+function css( element, property )
+{
+    return window.getComputedStyle(element, null).getPropertyValue(property);
+}
 
 function changeStyle(style)
 {
-    defaultColor();
     switch (style)
     {
         case "young":
@@ -39,33 +33,10 @@ function changeStyle(style)
         case "elegant":
             DOM.body.className = "elegant";
             break;
+        default:
+            DOM.body.className = "reset-style";
     }
 
     let option = document.querySelector(`#style option[value=""]`);
     option.selected = true;
-}
-
-function defaultColor()
-{
-    DOM.body.style.color = body_color;
-    DOM.body.style.backgroundColor = body_backcolor;
-    DOM.header.style.backgroundColor = header_backcolor;
-    DOM.header.style.color = header_color;
-    DOM.nav.style.backgroundColor = nav_color;
-    DOM.aside.style.backgroundColor = aside_color;
-    DOM.main.style.backgroundColor = main_color;
-    DOM.footer.style.backgroundColor = footer_color;
-    for (let i = 0; i < DOM.anchor.length; i++)
-    {
-        DOM.anchor[i].style.color = body_color;
-    }
-    for (let i = 0; i < DOM.list.length; i++)
-    {
-        DOM.list[i].style.color = body_color;
-        DOM.list[i].style.backgroundColor = body_backcolor;
-    }
-    // for (let i = 0; i < DOM.button.length; i++)
-    // {
-    //     DOM.button[i].style.background = "#198754"
-    // }
 }
