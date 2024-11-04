13e8206a8b641fd6a6242e5bdeb047ff88179f07
Orion68 <orions68@gmail.com>
2024-11-04 19:06:59 +0000

    Update 4-11-2024 19:06

---
+++ Client/Node-JS/Ejercicio-1/WishList/src/App.jsx
@@ -0,0 +1,32 @@
+import React  from 'react'
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+const wishes = [
+    {wish: "Vivir en Europa Como mis Antepasados", done: true},
+    {wish: "Ser uno de los Mejores Programadores del Mundo", done: false},
+    {wish: "Viajar a Argentina en 2025", done: false},
+    {wish: "Viajar a Júpiter", done: false}
+];
+
+const App = () => <div>
+    <h1>Lista de Deseos</h1>
+    <fieldset>
+        <legend>Nuevo Deseo</legend>
+        <input placeholder='Escribe tu Deseo'/>
+    </fieldset>
+    <ul>
+        {wishes.map(({wish, done}, i) => (
+            <li>
+                <label htmlFor={`wish$[i]`}>
+                    <input id={`wish${i}`} type='checkbox' checked={done}/>
+                    {wish}
+                </label>
+            </li>))}
+    </ul>
+    <button>Guarda tu Deseo</button>
+</div>
+
+export default App
