5cda60a53381ac0a4cba5d29556307ea5e770b2e
matelat-daw <cesarmatelat@gmail.com>
2024-11-08 12:54:30 +0000

    Update 8-11-2024 12:54

---
+++ Interface/Entrega-8-11-2/js/script.js
@@ -0,0 +1,61 @@
+const DOM = {
+    body: document.body,
+    mode: document.getElementById("mode"),
+    article: document.getElementById("article")
+};
+
+if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) // Verifica si El Navegador Soporta MatchMedia y si está Configurado el Modo dark.
+{
+    DOM.body.className = "dark"; // True, Pone la Página en modo Dark.
+    DOM.article.src = "imgs/kiwi-dark.jpg";
+}
+else
+{
+    DOM.body.className = "light"; // False, Pone la Página en Modo Light.
+}
+
+function changeStyle(style) // Cambia los Estilos de la Página, Según se Seleccione en el Selector y Dependoendo de como esté el Switch(Normal/Alto Contraste).
+{
+    switch (style)
+    {
+        case "dark":
+            DOM.article.src = "imgs/kiwi-dark.jpg";
+            DOM.body.className = "dark";
+            if (DOM.mode.checked) // Si el Switch (Normal/Alto Contraste) está Seleccionado.
+            {
+                DOM.body.className = "dark-hight"; // Activa el Modo Alto Contraste del Estilo Seleccionado
+            }
+            break;
+        case "light":
+            DOM.article.src = "imgs/kiwi.jpg";
+            DOM.body.className = "light";
+            if (DOM.mode.checked)
+            {
+                DOM.body.className = "light-hight";
+            }
+            break;
+    }
+
+    let option = document.querySelector(`#style option[value=""]`);
+    option.selected = true;
+}
+
+function changeMode() // Cambia Entre Normal y Alto Contraste.
+{
+    if (DOM.mode.checked && DOM.body.className == "dark")
+    {
+        DOM.body.className = "dark-hight";
+    }
+    else if (DOM.mode.checked && DOM.body.className == "light")
+    {
+        DOM.body.className = "light-hight";
+    }
+    else if (!DOM.mode.checked && DOM.body.className == "light-hight")
+    {
+        DOM.body.className = "light";
+    }
+    else
+    {
+        DOM.body.className = "dark";
+    }
+}
