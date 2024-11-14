ac4582c9aa2002c0669272b0f6631a6bbacb3543
matelat-daw <cesarmatelat@gmail.com>
2024-11-14 12:12:05 +0000

    Update 14-11-2024 12:11

---
+++ Client/Examen-20-11/DOM.js
@@ -0,0 +1,9 @@
+// Funciones del DOM.
+
+// Si se selecciona un Elemento en el inspector es $0.
+
+// let li = document.createElement("li");
+
+// $0.prepend(li); o $0.before(li); o $0.append(li); o $0.after(li);
+
+// $0.remove.(li);
