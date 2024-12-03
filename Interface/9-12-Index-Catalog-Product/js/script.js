5dcc0f4df18eb2dad2a168870074b0e6586a62b5
matelat-daw <cesarmatelat@gmail.com>
2024-12-03 09:49:42 +0000

    Update 3-12-2024 9:49

--- Interface/9-12-Index-Catalog-Product/js/script.js
+++ Interface/9-12-Index-Catalog-Product/js/script.js
@@ -1,18 +1,31 @@
 const DOM = {
     body: document.body,
     mode: document.getElementById("mode"),
-    article: document.getElementById("article")
+    article: document.getElementById("article"),
+    dark: document.getElementById("darkMode"),
+    light: document.getElementById("lightMode")
 };
-
-localStorage.setItem("style") = "";
 
 if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) // Verifica si El Navegador Soporta MatchMedia y si está Configurado el Modo dark.
 {
-    DOM.body.className = "dark"; // True, Pone la Página en modo Dark.
-    DOM.article.src = "../img/kiwi-dark.jpg";
+    console.log("El Modo por Defect es: " + localStorage.getItem("style"));
+    if (localStorage.getItem("style") == "dark")
+    {
+        localStorage.setItem("style", "dark");
+        DOM.dark.style.display = "none";
+        DOM.body.className = "dark"; // True, Pone la Página en modo Dark.
+        if (DOM.article != null)
+            DOM.article.src = "../img/kiwi-dark.jpg";
+    }
+    else
+    {
+        DOM.light.style.display = "none";
+        DOM.body.className = "light"; // False, Pone la Página en Modo Light.
+    }
 }
 else
 {
+    DOM.light.style.display = "none";
     DOM.body.className = "light"; // False, Pone la Página en Modo Light.
 }
 
@@ -21,66 +34,97 @@ function changeStyle(style) // Cambia los Estilos de la Página, Según se Selec
     switch (style)
     {
         case "dark":
-            DOM.article.src = "../img/kiwi-dark.jpg";
+            if (DOM.article != null)
+                DOM.article.src = "../img/kiwi-dark.jpg";
+            DOM.light.style.display = "block";
+            DOM.dark.style.display = "none";
             DOM.body.className = "dark";
-            if (DOM.mode.checked) // Si el Switch (Normal/Alto Contraste) está Seleccionado.
-            {
-                DOM.body.className = "dark-hight"; // Activa el Modo Alto Contraste del Estilo Seleccionado
-            }
             break;
         case "light":
-            DOM.article.src = "../img/kiwi.jpg";
+            if (DOM.article != null)
+                DOM.article.src = "../img/kiwi.jpg";
+            DOM.dark.style.display = "block";
+            DOM.light.style.display = "none";
             DOM.body.className = "light";
-            if (DOM.mode.checked)
-            {
-                DOM.body.className = "light-hight";
-            }
             break;
     }
-    localStorage.setItem("style") = style;
-
-    // let option = document.querySelector(`#style option[value=""]`);
-    // option.selected = true;
+    localStorage.setItem("style", style);
 }
 
-function changeMode(mode) // Cambia Entre Normal y Alto Contraste.
+var size = 2;
+
+function changeSize(where)
 {
-    switch (mode)
+    const fontSizes = ["small", "medium", "large", "x-large", "xx-large"];
+
+    var style = window.getComputedStyle(DOM.body, null).getPropertyValue('font-size');
+    var fontSize = parseFloat(style);
+    console.log("Size de las Fuente del Body es: " + fontSize);
+
+    switch (where)
     {
-        case "high":
-            if (localStorage.getItem("style") == "dark")
+        case "up":
+            size += 1;
+            if (size <= 4)
             {
-                DOM.body.className = "dark-hight";
+                DOM.body.style.fontSize = fontSizes[size];
             }
             else
             {
-                DOM.body.className = "light-hight";
+                size -= 1;
             }
             break;
-        default:
-            if (localStorage.getItem("style") == "dark")
+        case "down":
+            size -= 1;
+            if (size >= 0)
             {
-                DOM.body.className = "dark";
+                DOM.body.style.fontSize = fontSizes[size];
             }
             else
             {
-                DOM.body.className = "light";
+                size += 1;
             }
-    }
-    if (DOM.mode.checked && DOM.body.className == "dark")
-    {
-        DOM.body.className = "dark-hight";
-    }
-    else if (DOM.mode.checked && DOM.body.className == "light")
-    {
-        DOM.body.className = "light-hight";
-    }
-    else if (!DOM.mode.checked && DOM.body.className == "light-hight")
-    {
-        DOM.body.className = "light";
-    }
-    else
-    {
-        DOM.body.className = "dark";
+            break;
     }
 }
+// function changeMode(mode) // Cambia Entre Normal y Alto Contraste.
+// {
+//     // switch (mode)
+//     // {
+//     //     case "high":
+//     //         if (localStorage.getItem("style") == "dark")
+//     //         {
+//     //             DOM.body.className = "dark-hight";
+//     //         }
+//     //         else
+//     //         {
+//     //             DOM.body.className = "light-hight";
+//     //         }
+//     //         break;
+//     //     default:
+//     //         if (localStorage.getItem("style") == "dark")
+//     //         {
+//     //             DOM.body.className = "dark";
+//     //         }
+//     //         else
+//     //         {
+//     //             DOM.body.className = "light";
+//     //         }
+//     // }
+//     // if (DOM.mode.checked && DOM.body.className == "dark")
+//     // {
+//     //     DOM.body.className = "dark-hight";
+//     // }
+//     // else if (DOM.mode.checked && DOM.body.className == "light")
+//     // {
+//     //     DOM.body.className = "light-hight";
+//     // }
+//     // else if (!DOM.mode.checked && DOM.body.className == "light-hight")
+//     // {
+//     //     DOM.body.className = "light";
+//     // }
+//     // else
+//     // {
+//     //     DOM.body.className = "dark";
+//     // }
+// }
