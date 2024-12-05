764fd1b5bfeb35f05e409fb80fe4b45ef72be22a
Orion68 <orions68@gmail.com>
2024-12-05 17:34:20 +0000

    Update 5-12-2024 17:34

--- Interface/9-12-Index-Catalog-Product/js/script.js
+++ Interface/9-12-Index-Catalog-Product/js/script.js
@@ -90,44 +90,8 @@ function changeSize(where)
             }
         }
 }
-// function changeMode(mode) // Cambia Entre Normal y Alto Contraste.
-// {
-//     // switch (mode)
-//     // {
-//     //     case "high":
-//     //         if (localStorage.getItem("style") == "dark")
-//     //         {
-//     //             DOM.body.className = "dark-hight";
-//     //         }
-//     //         else
-//     //         {
-//     //             DOM.body.className = "light-hight";
-//     //         }
-//     //         break;
-//     //     default:
-//     //         if (localStorage.getItem("style") == "dark")
-//     //         {
-//     //             DOM.body.className = "dark";
-//     //         }
-//     //         else
-//     //         {
-//     //             DOM.body.className = "light";
-//     //         }
-//     // }
-//     // if (DOM.mode.checked && DOM.body.className == "dark")
-//     // {
-//     //     DOM.body.className = "dark-hight";
-//     // }
-//     // else if (DOM.mode.checked && DOM.body.className == "light")
-//     // {
-//     //     DOM.body.className = "light-hight";
-//     // }
-//     // else if (!DOM.mode.checked && DOM.body.className == "light-hight")
-//     // {
-//     //     DOM.body.className = "light";
-//     // }
-//     // else
-//     // {
-//     //     DOM.body.className = "dark";
-//     // }
-// }
+
+function visit(url)
+{
+    window.open(url, "_self");
+}
