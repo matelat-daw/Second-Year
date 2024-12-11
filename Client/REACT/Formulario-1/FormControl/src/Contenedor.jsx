83fc8218cb556384f5b7cb69ffee345501e0a95f
matelat-daw <cesarmatelat@gmail.com>
2024-12-11 12:20:31 +0000

    Update 11-12-2024 12:20

--- Client/REACT/Formulario-1/FormControl/src/Contenedor.jsx
+++ Client/REACT/Formulario-1/FormControl/src/Contenedor.jsx
@@ -1,7 +1,18 @@
 // import React from "react";
+// import { useState } from 'react';
 import FrmPersona from './FrmPersona'
+
+let personas = [];
+
 export default function Contenedor() {
+    function addPersona(persona)
+    {
+        personas.push(persona);
+        console.log(personas);
+    }
     return (
-        <FrmPersona/>
+        // Lista de Personas (Tarea para Mañana)
+        //useState cuando cambie el tamaño del array personas.
+        <FrmPersona addPersona = {addPersona}/>
     )
 }
