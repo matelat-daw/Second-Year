9861c669e90f1b977da809f905facbe9ed3711fb
Orion68 <orions68@gmail.com>
2024-12-11 17:13:46 +0000

    Update 11-12-2024 17:13

--- Client/REACT/Formulario-1/FormControl/src/Contenedor.jsx
+++ Client/REACT/Formulario-1/FormControl/src/Contenedor.jsx
@@ -1,15 +1,18 @@
 // import React from "react";
-// import { useState } from 'react';
+import { useState } from 'react';
 import FrmPersona from './FrmPersona'
 
 let personas = [];
+let setPersona
+[personas, setPersona] = useState(personas);
 
 export default function Contenedor() {
     function addPersona(persona)
     {
         personas.push(persona);
-        console.log(personas);
+        setPersona({personas});
     }
+    console.log(personas);
     return (
         // Lista de Personas (Tarea para Mañana)
         //useState cuando cambie el tamaño del array personas.
