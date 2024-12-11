5d076f1ed01fb92cb3d5dbc40ca548f0f3a7a3ef
Orion68 <orions68@gmail.com>
2024-12-11 22:15:11 +0000

    Update 11-12-2024 22:15

--- Client/REACT/Formulario-1/FormControl/src/Contenedor.jsx
+++ Client/REACT/Formulario-1/FormControl/src/Contenedor.jsx
@@ -1,28 +1,17 @@
 // import React from "react";
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import FrmPersona from './FrmPersona'
 
 let personas = [];
-let length = 0;
 
 export default function Contenedor() {
 
     let setPersona
     [personas, setPersona] = useState(personas);
-    let setLength
-    [length, setLength] = useState(length);
+
     function addPersona(persona)
     {
-        personas.push(persona);
-        useEffect(() => {
-            if (personas.length !== length)
-            {
-              setLength(personas.length);
-              console.log("Array length changed to:", personas.length);
-            }
-          }, [personas, length]);
-          // setPersona((personas) => [...personas, `Item ${personas.length + 1}`]);
-          setPersona((personas) => [...personas, `Item ${length}`]);
+        setPersona((personas)=>[...personas, persona])
     }
     console.log(personas);
     return (
