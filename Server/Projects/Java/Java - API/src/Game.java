3371acdfef51fc528d280576ccc0be06148e86ab
matelat-daw <cesarmatelat@gmail.com>
2024-12-16 00:54:30 +0000

    Update 16-12-2024 0:55

---
+++ Server/Projects/Java/Java - API/src/Game.java
@@ -0,0 +1,24 @@
+public class Game {
+    private String Name;
+    private Double Price;
+
+    public String GetName()
+    {
+        return this.Name;
+    }
+
+    public void SetName(String Name)
+    {
+        this.Name = Name;
+    }
+
+    public Double GetPrice()
+    {
+        return this.Price;
+    }
+
+    public void SetPrice(Double Price)
+    {
+        this.Price = Price;
+    }
+}
