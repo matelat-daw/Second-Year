7518826236b6c3a2aa7e838bd11f10623ef3bcd1
Orion68 <orions68@gmail.com>
2025-01-10 18:29:44 +0000

    Update 10-1-2025 18:29

--- Client/Promises/AsyncAwait/js/script.js
+++ Client/Promises/AsyncAwait/js/script.js
@@ -1,26 +1,80 @@
-function hola()
+const DOM =
 {
-    let html = "<h1>Hola Mundo!</h1>";
-    setTimeout(document.body.innerHTML = html, 5000);
-}
+    result: document.getElementById("result"),
+};
 
-var contadorDeClicks = 0;
+let contadorDeClicks = 0;
+
+document.addEventListener("click", ()=>{
+    contadorDeClicks++;
+    console.log("CLICK!!");
+});
 
 let promesa4Clicks;
+let html = "";
 
 function prometer4Clicks()
 {
-    let promesa = new Promise (fucntion(llamarAlThen, llamarAlCatch)
+    html += "2)-. Dispones de 5 Segundos Para Cumplirla.<br>";
+    result.innerHTML = html;
+    let promesa = new Promise (function(llamarAlThen, llamarAlCatch)
     {
-        if (contadorDeClicks >= 4)
-            llamarAlThen("3Sí, Lo has Conseguido.");
-        else
-            llamarAlCatch("3No, No has cumplido tu Promesa.")
-        setTimeout(comprobarPormesa, 5000);
-    });
+        function comprobarPromesa()
+        {
+            if (contadorDeClicks >= 4)
+                llamarAlThen(html += "3)-. Sí, ¡¡Muy Bien!! ... Has Cumplido tu Promesa de Hacer 4 Clicks en 5 Segundos.<br><br>")
+            else
+                llamarAlCatch(html += "3)-. No, ¡¡VAYA!! NO has Cumplido tu Promesa de Hacer 4 Clicks en 5 Segundos.<br><br>")
+        }
+        setTimeout(comprobarPromesa, 5000);
+        setTimeout(iniciarPromesa4Teclas, 5000);
+    })
+    return promesa;
 }
 
 function iniciarPromesa4Clicks()
 {
+    html += "1)-. Iniciar Promesa de Hacer 4 Clicks.<br>";
+    result.innerHTML = html;
+    promesa4CLicks = prometer4Clicks()
+    .then(respuesta=>result.innerHTML = html)
+    .catch(respuesta=>result.innerHTML = html)
+}
 
+iniciarPromesa4Clicks();
+
+let contadorDeTeclas = 0;
+
+document.addEventListener("keypress", ()=>{
+    contadorDeTeclas++;
+    console.log("Tecla Pulsada!!");
+});
+
+let promesa4Teclas;
+
+function prometer4Teclas()
+{
+    html += "5)-. Dispones de 5 Segundos Para Cumplirla.<br>";
+    result.innerHTML = html;
+    let promesa = new Promise (function(llamarAlThen, llamarAlCatch)
+    {
+        function comprobarPromesa()
+        {
+            if (contadorDeTeclas >= 4)
+                llamarAlThen(html += "6)-. Sí, ¡¡Muy Bien!! ... Has Cumplido tu Promesa de Pulsar 4 Teclas en 5 Segundos.");
+            else
+                llamarAlCatch(html += "6)-. No, ¡¡VAYA!! NO has Cumplido tu Promesa de Pulsar 4 Teclas en 5 Segundos.")
+        }
+        setTimeout(comprobarPromesa, 5000);
+    })
+    return promesa;
+}
+
+function iniciarPromesa4Teclas()
+{
+    html += "4)-. Iniciar Promesa de Pulsar 4 Teclas.<br>";
+    result.innerHTML = html;
+    promesa4Teclas = prometer4Teclas()
+    .then(respuesta=>result.innerHTML = html)
+    .catch(respuesta=>result.innerHTML = html)
 }
