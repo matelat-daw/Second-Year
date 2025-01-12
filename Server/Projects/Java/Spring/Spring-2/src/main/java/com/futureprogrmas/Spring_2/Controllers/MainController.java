b279e173fb81250293c1d5560df42c47e84755bf
Orion68 <orions68@gmail.com>
2025-01-12 00:04:45 +0000

    dale

--- Server/Projects/Java/Spring/Spring-2/src/main/java/com/futureprogrmas/Spring_2/Controllers/MainController.java
+++ Server/Projects/Java/Spring/Spring-2/src/main/java/com/futureprogrmas/Spring_2/Controllers/MainController.java
@@ -1,4 +1,4 @@
-package Controllers;
+package com.futureprogrmas.Spring_2.Controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
---
+++ Server/Projects/Java/Spring/Spring-2/src/test/java/com/futureprogrmas/Spring_2/Spring2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.futureprogrmas.Spring_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
