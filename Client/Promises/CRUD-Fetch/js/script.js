3489c21bcdc1a16c672093385ecf907242f613a2
matelat-daw <cesarmatelat@gmail.com>
2025-01-16 10:47:31 +0000

    Update 16-1-2025 10:47

--- Client/Promises/CRUD-Fetch/js/script.js
+++ Client/Promises/CRUD-Fetch/js/script.js
@@ -3,20 +3,22 @@ const DOM = {
     result2: document.getElementById("result2"),
     result3: document.getElementById("result3"),
     result4: document.getElementById("result4"),
-    result5: document.getElementById("result5"),
     resultError: document.getElementById("resultError"),
     name: document.getElementById("name"),
     grupo: document.getElementById("grupo"),
     hidden: document.getElementById("hidden")
 }
 
+DOM.grupo.option.value = fetch("http://localhost:3000/grupos")
+
 function unhide()
 {
     DOM.hidden.style.display = "block";
 }
 
-function checkForm()
+function checkForm(e)
 {
+    e.preventDefault();
     if (DOM.name.value != "" && DOM.grupo.value != "")
     {
         crudCreate();
@@ -33,7 +35,6 @@ function crudRead()
 {
     fetch("http://localhost:3000/alumnos").then(respuesta => respuesta.json())
                                         .then(jsonData => result.innerHTML = JSON.stringify(jsonData));
-    crudRead();
 }
 
 function crudCreate()
@@ -51,32 +52,31 @@ function crudCreate()
     }).then(res => res.json())
     .catch(error => resultError.innerHTML = "Error al Crear: " + error)
     .then(response => result2.innerHTML = "Se ha Creado el Alumno : " + JSON.stringify(response));
-    crudRead();
 }
 
-function crudUpdate()
+function crudUpdate(id)
 {
     datosUpdate = {
-        nombre: "Evangelina",
-        grupo: "A"
+        nombre: DOM.name.value,
+        grupo: DOM.grupo.value
     }
-    fetch("http://localhost:3000/alumnos/2", {
+    fetch("http://localhost:3000/alumnos/" + id, {
         method: "PATCH",
         body: JSON.stringify(datosUpdate),
         headers: {
             "Content-Type": "application/json"
         },
     }).then(res => res.json())
-    .catch(error => resultError.innerHTML = "Error al Actalizar: " + error)
+    .catch(error => resultError.innerHTML = "Error al Actualizar: " + error)
     .then( response => result3.innerHTML = "Se ha Actualizado el Usaurio: " + response)
 }
 
-function crudDelete()
+function crudDelete(id)
 {
-    fetch("http://localhost:3000/alumnos/3", {
+    fetch("http://localhost:3000/alumnos/" + id, {
         method: "DELETE"
         
     }).then(res => res.json)
-    .catch(error => resultError.innerHTML = "Ha Habido un Error: " + error)
+    .catch(error => resultError.innerHTML = "Error al Intentar Eliminar: " + error)
     .then(respuesta => result4.innerHTML = "Se ha Eliminado el Alumno.")
 }
