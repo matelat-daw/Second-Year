62f7569ec92b52ea4da2492e0f7083acdb068891
Orion68 <orions68@gmail.com>
2025-01-18 11:13:09 +0000

    Update 18-1-2025 11:13

--- Client/Promises/CRUD-Fetch/js/script.js
+++ Client/Promises/CRUD-Fetch/js/script.js
@@ -4,16 +4,23 @@ const DOM = {
     update: document.getElementById("update"),
     delete: document.getElementById("delete"),
     id: document.getElementById("id"),
-    name: document.getElementById("name"),
-    grupo: document.getElementById("grupo")
+    nombre: document.getElementById("nombre"),
+    grupo: document.getElementById("grupo"),
+    alertaDelete: document.getElementById("alertaDelete"),
+    titleDelete: document.getElementById("titleDelete"),
+    messageDelete: document.getElementById("messageDelete"),
+    alerta: document.getElementById("alerta"),
+    title: document.getElementById("title"),
+    message: document.getElementById("message"),
+    update: document.getElementById("updateIt")
 }
 
-document.addEventListener("DOMContentLoaded", () => { // Listener que es Llamado Cuando se Carga el DOM
+// document.addEventListener("DOMContentLoaded", () => { // Listener que es Llamado Cuando se Carga el DOM
     crudRead();
     fetch("http://localhost:3000/grupos").then(respuesta => respuesta.json())
         .then(grupos => populateSelect(grupos))
         .catch(error => toast(2, "ERROR:", "Ha Ocurrido el Error: " + error)); // Este fetch Solicita el Objeto Grupos al Servidor, y si Todo es Correcto le pasa el Objeto con los Grupos a la Función populateSelect(grupos).
-  });
+//   });
 
 function populateSelect(options) // Rellena las Opciones del Select al Cargar la Página, Recibe el Objeto con los Grupos.
 {
@@ -39,8 +46,7 @@ function checkForm(e) // Al Pulsar Cualquier de los Botónes Llama a este Métod
     {
         if (DOM.id.value != "") // Verifico si el Campo ID NO está Vacio.
         {
-            deleteAlumno("Estas Seguro que Quieres Eliminar el Usuario con ID: " + DOM.id.value);
-            // crudDelete(DOM.id.value); // Si no Está Vacio Llamo al Método crudDelete, Pasandole como Parámetro la ID (El Valor del Campo con ID id).
+            deleteAlumno("Se Eliminara un Alumno:", "Estas Seguro que Quieres Eliminar el Usuario con ID: " + DOM.id.value); // Muestra el Diálogo de Confirmación Para Eliminar, se Puede Cancelar o Acaptar.
         }
         else // Si el Campo Está Vacio.
         {
@@ -53,18 +59,25 @@ function verify(what) // Método para Verificar si se Pulsó Create o Update, Re
 {
     if (what == "create") // Si la String Recivida es create.
     {
-        if (DOM.id.value == "" && DOM.name.value != "" && DOM.grupo.value != "") // Verifico los Campos del Formulario, Si la ID Está Vacia y los Campos Nombre y Grupo Tienen Datos.
+        if (DOM.id.value == "") // Verifico los Campos del Formulario, Si la ID Está Vacia.
         {
-            crudCreate(); // Llamo al Método crusCreate()
+            if (DOM.nombre.value != "" && DOM.grupo.value != "") //  Verifico si los Campos Nombre y Grupo Tienen Datos
+            {
+                crudCreate(); // Llamo al Método crudCreate().
+            }
+            else // Si No.
+            {
+                toast(1, "Falta Datos:", "¿Te Dejaste Algún Campo en Blanco? Verifica que Hayas Rellenado Todos los Datos"); // Mensaje de Error.
+            }
         }
         else // Si No.
         {
-            toast(1, "Faltan Datos:", "¿Te Dejaste Algún Campo en Blanco? o Quisas Escribiste una ID, tal Vez Estés Intentando Actualizar un Alumno. Verifica que Hayas Rellenado Todos los Datos y que Hayas Pulsado el Botón Correcto. Gracias"); // Mensaje de Error.
+            toast(1, "Actualizas Datos:", "Escribiste una ID, ¿Tal Vez Estés Intentando Actualizar un Alumno? Verifica si Has Pulsado el Botón Correcto. Gracias"); // Mensaje de Error.
         }
     }
-    else // Si la String Recibida no es create.
+    else // Si la String Recibida no es create(es update).
     {
-        if (DOM.id.value != "" && DOM.name.value != "" && DOM.grupo.value != "") // Verifico los Campos del Formulario, Si la ID Tiene Datos y los Campos Nombre y Grupo Tienen Datos.
+        if (DOM.id.value != "" && DOM.nombre.value != "" && DOM.grupo.value != "") // Verifico los Campos del Formulario, Si Todos los Campos Tienen Datos.
         {
             crudUpdate(DOM.id.value); // Llamo al Método crudUpdate, Pasandole Como Parametro la ID del Campo id del Formulario.
         }
@@ -78,24 +91,40 @@ function verify(what) // Método para Verificar si se Pulsó Create o Update, Re
 async function crudRead()
 {
     await fetch("http://localhost:3000/alumnos").then(respuesta => respuesta.json())
-                                        .then(jsonData => drawTable(jsonData));
+                                                .catch(respuesta => toast(2, "Error de Conexión", "Lo Siento No hay Conexión con el Servidor. Asegurate de que el Servidor está en Ejecución. Error" + respuesta))
+                                                .then(jsonData => drawTable(jsonData)); // Después de Leer los Datos del Servidor, Llamo a la función drawTable, Pasandole por Parametro los Datos.
 }
 
-function drawTable(data)
+function drawTable(data) // Dibuja la Tabla con los Datos del Servidor.
 {
-    let head = "<thead><tr><th>ID</th><th>Nombre</th><th>Grupo</th></tr></thead>";
+    let head = "<thead><tr><th>ID</th><th>Nombre</th><th>Grupo</th><th>Acciones</th></tr></thead>";
     let body = "";
     data.map(alumno => {
-        body += `<tr><td> ${alumno.id} </td><td> ${alumno.nombre} </td><td> ${alumno.grupo} </td></tr>`;
+        body += `<tr><td> ${alumno.id} </td><td> ${alumno.nombre} </td><td> ${alumno.grupo} </td><td><input id="update" type="button" onclick="populateForm(${alumno.id}, '${alumno.nombre}', '${alumno.grupo}')" value="Actulizar Alumno" class="btn btn-primary">&nbsp;<input id="delete" type="button" onclick="crudDelete(${alumno.id})" value="Eliminar Alumno" class="btn btn-danger"></td></tr>`;
     });
 
     table.innerHTML = head.concat(body);
 }
 
+function populateForm(id, nombre, grupo)
+{
+    DOM.id.value = id;
+    DOM.nombre.value = nombre;
+    DOM.grupo.value = grupo;
+    DOM.update.style.display = "block";
+}
+
+function formClean()
+{
+    DOM.id.value = "";
+    DOM.nombre.value = "";
+    DOM.grupo.value = "";
+}
+
 function crudCreate()
 {
     let datosCreate = {
-        nombre: DOM.name.value,
+        nombre: DOM.nombre.value,
         grupo: DOM.grupo.value
     }
     fetch("http://localhost:3000/alumnos", {
@@ -106,14 +135,14 @@ function crudCreate()
         },
     }).then(res => res.json())
     .catch(error => toast(2, "", "Error al Crear: " + error))
-    .then(response1 => toast(0, "Alumno Nuevo:", "Se Ha Agregado un Nuevo Alumno a la Base de Datos." + response1))
-    .then(responce2 => console.log("Se Ha Creado el Alumno: " + responce2));
+    // .then(response1 => toast(0, "Alumno Nuevo:", "Se Ha Agregado un Nuevo Alumno a la Base de Datos." + response1))
+    .then(responce => formClean());
 }
 
 function crudUpdate(id)
 {
     datosUpdate = {
-        nombre: DOM.name.value,
+        nombre: DOM.nombre.value,
         grupo: DOM.grupo.value
     }
     fetch("http://localhost:3000/alumnos/" + id, {
@@ -124,7 +153,8 @@ function crudUpdate(id)
         },
     }).then(res => res.json())
     .catch(error => resultError.innerHTML = "Error al Actualizar: " + error)
-    .then( response => console.log("Se ha Actualizado el Usaurio: " + response))
+    .then(responce => formClean());
+    // .then( response => console.log("Se ha Actualizado el Usaurio: " + response))
 }
 
 function crudDelete(id)
@@ -140,34 +170,29 @@ function crudDelete(id)
 
 function toast(warn, ttl, msg) // Función para mostrar el Dialogo con los mensajes de alerta, recibe, Código, Título y Mensaje.
 {
-    var alerta = document.getElementById("alerta"); // La ID del botón del dialogo.
-    var title = document.getElementById("title"); // Asigno a la variable title el h4 con id title.
-    var message = document.getElementById("message"); // Asigno a la variable message el h5 con id message;
     if (warn == 1) // Si el código es 1, es una alerta.
     {
-        title.style.backgroundColor = "#000000"; // Pongo los atributos, color de fondo negro.
-        title.style.color = "yellow"; // Y color del texto amarillo.
+        DOM.title.style.backgroundColor = "#000000"; // Pongo los atributos, color de fondo negro.
+        DOM.title.style.color = "yellow"; // Y color del texto amarillo.
     }
     else if (warn == 0) // Si no, si el código es 0 es un mensaje satisfactorio.
     {
-        title.style.backgroundColor = "#FFFFFF"; // Pongo los atributos, color de fondo blanco.
-        title.style.color = "blue"; // Y el color del texto azul.
+        DOM.title.style.backgroundColor = "#FFFFFF"; // Pongo los atributos, color de fondo blanco.
+        DOM.title.style.color = "blue"; // Y el color del texto azul.
     }
     else // Si no, viene un 2, es una alerta de error.
     {
-        title.style.backgroundColor = "#000000"; // Pongo los atributos, color de fondo negro.
-        title.style.color = "red"; // Y color del texto rojo.
+        DOM.title.style.backgroundColor = "#000000"; // Pongo los atributos, color de fondo negro.
+        DOM.title.style.color = "red"; // Y color del texto rojo.
     }
-    title.innerHTML = ttl; // Muestro el Título del dialogo.
-    message.innerHTML = msg; // Muestro los mensajes en el diálogo.
-    alerta.click(); // Lo hago aparecer pulsando el botón con ID alerta.
+    DOM.title.innerHTML = ttl; // Muestro el Título del dialogo.
+    DOM.message.innerHTML = msg; // Muestro los mensajes en el diálogo.
+    DOM.alerta.click(); // Lo hago aparecer pulsando el botón con ID alerta.
 }
 
-function deleteAlumno(msg)
+function deleteAlumno(ttl, msg)
 {
-    var alertaDelete = document.getElementById("alertaDelete");
-    var message = document.getElementById("message2");
-
-    message.innerHTML = msg; // Muestro los mensajes en el diálogo.
-    alertaDelete.click(); // Lo hago aparecer pulsando el botón con ID alertaDelete.
+    DOM.titleDelete.innerHTML = ttl; // Muestro los mensajes en el diálogo.
+    DOM.messageDelete.innerHTML = msg;
+    DOM.alertaDelete.click(); // Lo hago aparecer pulsando el botón con ID alertaDelete.
 }
