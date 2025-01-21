ea6320aec520b16e044ea73b71d022f170fe251c
Orion68 <orions68@gmail.com>
2025-01-21 22:42:06 +0000

    Update 21-1-2025 22:42

---
+++ Client/13-11-Tema1-Tema2/Corregido de EVAGD/script.js
@@ -0,0 +1,24 @@
+let strMunicipios='[{"municipio":"Adeje","poblacion":47869,"isla":"Tenerife"},{"municipio":"Agulo","poblacion":1096,"isla":"La Gomera"},{"municipio":"Alajeró","poblacion":2017,"isla":"La Gomera"},{"municipio":"Arafo","poblacion":5551,"isla":"Tenerife"},{"municipio":"Arico","poblacion":7988,"isla":"Tenerife"},{"municipio":"Arona","poblacion":81216,"isla":"Tenerife"},{"municipio":"Barlovento","poblacion":1876,"isla":"La Palma"},{"municipio":"Breña Alta","poblacion":7204,"isla":"La Palma"},{"municipio":"Breña Baja","poblacion":5690,"isla":"La Palma"},{"municipio":"Buenavista del Norte","poblacion":4778,"isla":"Tenerife"},{"municipio":"Candelaria","poblacion":27985,"isla":"Tenerife"},{"municipio":"Fasnia","poblacion":2786,"isla":"Tenerife"},{"municipio":"Frontera","poblacion":4093,"isla":"El Hierro"},{"municipio":"Fuencaliente de la Palma","poblacion":1722,"isla":"La Palma"},{"municipio":"Garachico","poblacion":4871,"isla":"Tenerife"},{"municipio":"Garafía","poblacion":1667,"isla":"La Palma"},{"municipio":"Granadilla de Abona","poblacion":50146,"isla":"Tenerife"},{"municipio":"La Guancha","poblacion":5520,"isla":"Tenerife"},{"municipio":"Guía de Isora","poblacion":21368,"isla":"Tenerife"},{"municipio":"Güímar","poblacion":20190,"isla":"Tenerife"},{"municipio":"Hermigua","poblacion":1832,"isla":"La Gomera"},{"municipio":"Icod de los Vinos","poblacion":23254,"isla":"Tenerife"},{"municipio":"Los Llanos de Aridane","poblacion":20462,"isla":"La Palma"},{"municipio":"La Matanza de Acentejo","poblacion":9061,"isla":"Tenerife"},{"municipio":"La Orotava","poblacion":42029,"isla":"Tenerife"},{"municipio":"El Paso","poblacion":7622,"isla":"La Palma"},{"municipio":"El Pinar de El Hierro","poblacion":1870,"isla":"El Hierro"},{"municipio":"Puerto de la Cruz","poblacion":30468,"isla":"Tenerife"},{"municipio":"Puntagorda","poblacion":2110,"isla":"La Palma"},{"municipio":"Puntallana","poblacion":2506,"isla":"La Palma"},{"municipio":"Los Realejos","poblacion":36402,"isla":"Tenerife"},{"municipio":"El Rosario","poblacion":17370,"isla":"Tenerife"},{"municipio":"San Andrés y Sauces","poblacion":4141,"isla":"La Palma"},{"municipio":"San Cristóbal de La Laguna","poblacion":157503,"isla":"Tenerife"},{"municipio":"San Juan de la Rambla","poblacion":4828,"isla":"Tenerife"},{"municipio":"San Miguel de Abona","poblacion":20886,"isla":"Tenerife"},{"municipio":"San Sebastián de la Gomera","poblacion":9093,"isla":"La Gomera"},{"municipio":"Santa Cruz de La Palma","poblacion":15716,"isla":"La Palma"},{"municipio":"Santa Cruz de Tenerife","poblacion":207312,"isla":"Tenerife"},{"municipio":"Santa Úrsula","poblacion":14679,"isla":"Tenerife"},{"municipio":"Santiago del Teide","poblacion":11111,"isla":"Tenerife"},{"municipio":"El Sauzal","poblacion":8934,"isla":"Tenerife"},{"municipio":"Los Silos","poblacion":4693,"isla":"Tenerife"},{"municipio":"Tacoronte","poblacion":24134,"isla":"Tenerife"},{"municipio":"El Tanque","poblacion":2763,"isla":"Tenerife"},{"municipio":"Tazacorte","poblacion":4575,"isla":"La Palma"},{"municipio":"Tegueste","poblacion":11294,"isla":"Tenerife"},{"municipio":"Tijarafe","poblacion":2532,"isla":"La Palma"},{"municipio":"Valle Gran Rey","poblacion":4564,"isla":"La Gomera"},{"municipio":"Vallehermoso","poblacion":2901,"isla":"La Gomera"},{"municipio":"Valverde","poblacion":5005,"isla":"El Hierro"},{"municipio":"La Victoria de Acentejo","poblacion":9185,"isla":"Tenerife"},{"municipio":"Vilaflor de Chasna","poblacion":1667,"isla":"Tenerife"},{"municipio":"Villa de Mazo","poblacion":4843,"isla":"La Palma"}]'
+let pilots = [
+    {
+      id: 10,
+      name: "Poe Dameron",
+      years: 14,
+    },
+    {
+      id: 2,
+      name: "Francisco Javier González Pérez",
+      years: 30,
+    },
+    {
+      id: 41,
+      name: "Tallissan Lintra",
+      years: 16,
+    },
+    {
+      id: 99,
+      name: "Ello Asty",
+      years: 22,
+    }
+  ];
+  
