34a47e722edb175a54aa08c22bfbce35c917b54f
Orion68 <orions68@gmail.com>
2025-01-28 20:37:48 +0000

    Update 28-1-2025 20:37

--- Server/Projects/Java/Spring/AnimalDB/src/main/java/com/futureprograms/AnimalDB/Services/AnimalServiceDB.java
+++ Server/Projects/Java/Spring/AnimalDB/src/main/java/com/futureprograms/AnimalDB/Services/AnimalServiceDB.java
@@ -27,23 +27,23 @@ public class AnimalServiceDB {
     }
 
     public Animal findById(int id) {
-        return ai.findById(id); // Busca por ID
+        return ai.findById(id).orElse(null); // Busca por ID y maneja el caso de no encontrar el animal
     }
 
     public Animal save(Animal animal) {
-        ai.save(animal); // Guarda el nuevo animal en la base de datos
-        return animal;
+        return ai.save(animal); // Guarda el nuevo animal en la base de datos y retorna el animal guardado
     }
 
     public Animal animalUpdate(int id, Animal updatedAnimal) {
-        Animal existingAnimal = ai.findById(id);
-
-        // Actualiza los campos del animal existente
-        existingAnimal.setName(updatedAnimal.getName());
-        existingAnimal.setAge(updatedAnimal.getAge());
-        existingAnimal.setExtinct(updatedAnimal.getExtinct());
-
-        return ai.save(existingAnimal); // Guarda los cambios
+        Animal existingAnimal = ai.findById(id).orElse(null);
+        if (existingAnimal != null) {
+            // Actualiza los campos del animal existente
+            existingAnimal.setName(updatedAnimal.getName());
+            existingAnimal.setAge(updatedAnimal.getAge());
+            existingAnimal.setExtinct(updatedAnimal.getExtinct());
+            return ai.save(existingAnimal); // Guarda los cambios y retorna el animal actualizado
+        }
+        return null; // Retorna null si el animal no existe
     }
 
     public void deleteById(int id) {
